cd90301d6ad0d89edf86530820212932716bf46b
clibby2655 <christalyn.libby@yahoo.com>
2015-05-02 09:55:31 -0400

    Update Checking.java

--- src/personalfinanceproject/Checking.java
+++ src/personalfinanceproject/Checking.java
@@ -3,22 +3,47 @@ package personalfinanceproject;
 public class Checking {
 	private double balance;
 	
+	/**
+	 * Checking
+	 * sets the balance
+	 * @param balance
+	 */
 	public Checking(double balance) {
 		this.balance = balance;
 	}
-		
+	
+	/**
+	 * setChecking
+	 * sets the balance
+	 * @param balance
+	 */	
 	public void setChecking(double balance) {
 		this.balance = balance;
 	}
 	
+	/**
+	 * getChecking
+	 * returns the balance
+	 * @return balance
+	 */
 	public double getChecking() {
 		return balance;
 	}
 	
+	/**
+	 * checkingDeposit
+	 * adds a deposit to the balance
+	 * @param deposit
+	 */
 	public void checkingDeposit(double deposit) {
 		balance += deposit;
 	}
 	
+	/**
+	 * checkingWithdrawal
+	 * sets the new balance after a withdrawal
+	 * @param withdrawal
+	 */
 	public void checkingWithdrawal(double withdrawal) {
 		balance -= withdrawal;
 	}
