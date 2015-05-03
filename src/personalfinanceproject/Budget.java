12bf2d8d6a515c99ec752d35f8018fb5341ea312
clibby2655 <christalyn.libby@yahoo.com>
2015-05-02 20:52:31 -0400

    Update Budget.java

--- src/personalfinanceproject/Budget.java
+++ src/personalfinanceproject/Budget.java
@@ -5,7 +5,8 @@ package cis111B.personalfinanceproject;
 
 /**
  * @author patrickowens
- *
+ * Budget
+ * This class keeps track a users budget over a particular time frame
  */
 public class Budget {
 	
@@ -18,14 +19,29 @@ public class Budget {
 	public double netIncome;
 	public double totalExpenses;
 	
+	/**
+ 	* getIdealAmount
+ 	* gets the budget goal for a particular time frame
+ 	* @return amountGoal
+	*/
 	public double getIdealAmount() {
 		return amountGoal;
 	}
 	
+	/**
+ 	* gettimetoAchieve
+ 	* gets the particular time frame for a budget goal
+ 	* @return timeFrame
+	*/
 	public double gettimetoAchieve(){
 		return timeFrame;
 	}
 	
+	/**
+ 	* getRealAmount
+ 	* gets the amount left available after expenses
+ 	* @return (amountGoal-totalExpenses)
+	*/
 	public double getRealAmount(){
 		return (amountGoal - totalExpenses);
 	}
