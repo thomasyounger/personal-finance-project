c6ee97fd5acfcc36e9e4b3718ec9e5e9a235e515
clibby2655 <christalyn.libby@yahoo.com>
2015-05-02 21:01:08 -0400

    Update Savings.java

--- src/personalfinanceproject/Savings.java
+++ src/personalfinanceproject/Savings.java
@@ -1,24 +1,53 @@
 package personalfinanceproject;
 
+/**
+ * Savings
+ * This class keeps track of any savings, deposits, and withdrawals
+*/
 public class Savings {
 	private double balance;
 	
+	/**
+ 	* Savings
+ 	* sets the balance
+ 	* @parm balance
+	*/
 	public Savings(double balance) {
 		this.balance = balance;
 	}
-		
+	
+	/**
+ 	* setSavings
+ 	* sets the balance
+ 	* @parm balance
+	*/	
 	public void setSavings(double balance) {
 		this.balance = balance;
 	}
 	
+	/**
+ 	* getSavings
+ 	* gets the current savings total
+ 	* @return balance
+	*/
 	public double getSavings() {
 		return balance;
 	}
 	
+	/**
+ 	* savingsDeposit
+ 	* adds a deposit into the savings balance
+ 	* @parm deposit
+	*/
 	public void savingsDeposit(double deposit) {
 		balance += deposit;
 	}
 	
+	/**
+ 	* savingsWithdrawal
+ 	* sutracts any withdrawals from the savings balance
+ 	* @parm withdrawal
+	*/
 	public void savingsWithdrawal(double withdrawal) {
 		balance -= withdrawal;
 	}
