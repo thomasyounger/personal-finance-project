247944c187a1679dfa5ae627278f7f2935b5bd5d
clibby2655 <christalyn.libby@yahoo.com>
2015-05-02 20:48:06 -0400

    Update Rent.java

--- src/personalfinanceproject/Rent.java
+++ src/personalfinanceproject/Rent.java
@@ -5,11 +5,12 @@ package cis111B.personalfinanceproject;
 
 /**
  * @author thomasyounger
- *
+ * This class keeps track of monthly rent
  */
 public class Rent extends Expense {
 
 	/**
+	 * Rent
 	 * @param amount
 	 */
 	public Rent(double amount) {
