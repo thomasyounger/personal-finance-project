0379f14a6a5374d1fd488d81fb4d93684adb5f36
clibby2655 <christalyn.libby@yahoo.com>
2015-05-02 21:02:56 -0400

    Update Utilities.java

--- src/personalfinanceproject/Utilities.java
+++ src/personalfinanceproject/Utilities.java
@@ -5,11 +5,14 @@ package cis111B.personalfinanceproject;
 
 /**
  * @author thomasyounger
- *
+ * Utilities
+ *  This class keeps track of any utility expenses
  */
 public class Utilities extends Expense {
 
 	/**
+	 * Utilities
+	 * sets the utilities expenses
 	 * @param amount
 	 */
 	public Utilities(double amount) {
