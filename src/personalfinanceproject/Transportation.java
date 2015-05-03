38129607c1e94ae8942078be527e60be9fbecd1c
clibby2655 <christalyn.libby@yahoo.com>
2015-05-02 21:02:08 -0400

    Update Transportation.java

--- src/personalfinanceproject/Transportation.java
+++ src/personalfinanceproject/Transportation.java
@@ -5,11 +5,14 @@ package cis111B.personalfinanceproject;
 
 /**
  * @author thomasyounger
- *
+ * Transportation
+ * This class keeps track of transportation costs
  */
 public class Transportation extends Expense {
 
 	/**
+	 * Transportation
+	 * sets the transportation amount used
 	 * @param amount
 	 */
 	public Transportation(double amount) {
