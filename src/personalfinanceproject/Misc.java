c587b8e49ef172eafcb13a3e839e5001e702061f
clibby2655 <christalyn.libby@yahoo.com>
2015-05-02 20:39:35 -0400

    Update Misc.java

--- src/personalfinanceproject/Misc.java
+++ src/personalfinanceproject/Misc.java
@@ -5,11 +5,12 @@ package cis111B.personalfinanceproject;
 
 /**
  * @author thomasyounger
- *
+ * This class keeps track of the amount of funds being used for miscellaneous purposes
  */
 public class Misc extends Expense {
 
 	/**
+	 * Misc
 	 * @param amount
 	 */
 	public Misc(double amount) {
