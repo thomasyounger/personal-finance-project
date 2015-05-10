67ca1ab5c6804b8620a39aae7e5e309e66330468
clibby2655 <christalyn.libby@yahoo.com>
2015-05-10 13:45:03 -0400

    Update Medical.java

--- src/personalfinanceproject/Medical.java
+++ src/personalfinanceproject/Medical.java
@@ -1,16 +1,17 @@
-/**
- * 
- */
 package cis111B.personalfinanceproject;
 
 /**
  * @author thomasyounger
+ * javadoc by christalyn
+ * Medical
+ * @inheritDoc Expense
  * This class keeps track of medical expenses
  */
 public class Medical extends Expense {
 
 	/**
 	 * Medical
+	 * inherits the amount
 	 * @param amount
 	 */
 	public Medical(double amount) {
