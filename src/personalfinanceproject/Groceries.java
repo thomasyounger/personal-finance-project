0f8d4c53d907af06c0cb69fed4b4579467634b11
clibby2655 <christalyn.libby@yahoo.com>
2015-05-10 13:27:55 -0400

    Update Groceries.java

--- src/personalfinanceproject/Groceries.java
+++ src/personalfinanceproject/Groceries.java
@@ -1,16 +1,17 @@
-/**
- * 
- */
 package cis111B.personalfinanceproject;
 
 /**
  * @author thomasyounger
+ * javadoc by christalyn
+ * Groceries
+ * @inheritDoc Expense
  * This class keeps track of groceries/food expenses
  */
 public class Groceries extends Expense {
 
 	/**
 	 * Groceries
+	 * inherits the amount
 	 * @param amount
 	 */
 	public Groceries(double amount) {
