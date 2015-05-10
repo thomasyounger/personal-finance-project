2dd902b18dcba61b05562254a441153d73116b04
clibby2655 <christalyn.libby@yahoo.com>
2015-05-10 13:18:41 -0400

    Update Cash.java

--- src/personalfinanceproject/Cash.java
+++ src/personalfinanceproject/Cash.java
@@ -1,16 +1,16 @@
-/**
- * 
- */
 package cis111B.personalfinanceproject;
 
 /**
  * @author thomasyounger
+ * @inheritDoc Expense
+ * javadoc by christalyn
  * This class keeps track of the amount of cash on a person at any given time
  */
 public class Cash extends Expense {
 
 	/**
 	 * Cash
+	 * inheriting amount
 	 * @param amount
 	 */
 	public Cash(double amount) {
