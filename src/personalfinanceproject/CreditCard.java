2e2108f00b9aa6ca953796b55cc7a7652671f15d
clibby2655 <christalyn.libby@yahoo.com>
2015-05-10 13:21:23 -0400

    Update CreditCard.java

--- src/personalfinanceproject/CreditCard.java
+++ src/personalfinanceproject/CreditCard.java
@@ -1,16 +1,17 @@
-/**
- * 
- */
 package cis111B.personalfinanceproject;
 
 /**
  * @author thomasyounger
+ * javadoc by christalyn
+ * CreditCard
+ * @inheritDoc Expense
  * This class keeps track of purchases made on a creditcard
  */
 public class CreditCard extends Expense {
 
 	/**
 	 * CreditCard
+	 * inherits the amount
 	 * @param amount
 	 */
 	public CreditCard(double amount) {
